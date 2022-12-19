0ea8066642692d6693d5bead5dd90558872d792e
Toma Adrian Ionut <sirhadrian21@gmail.com>
2022-12-19 18:04:15 +0200

    improved archiving script

--- archive.sh
+++ archive.sh
@@ -1,17 +1,40 @@
 #!/usr/bin/bash
 
-archive(){
-    echo ".............Start archiving $1............."
-    tar -czvf $(basename "$1").tar.gz "$1"
-    echo ".............Done archiving $1............."
+archive() {
+	echo ".............Start archiving $1............."
+	tar -czvf "$(basename "$1")".tgz "$1"
+	echo ".............Done archiving $1............."
 }
 
-echo "==================== ARCHIVING: $@ ===================="
+run() {
+	for item in "$@"; do
+		archive "$item" &
+	done
 
-for item in "$@"
-do
-    archive "$item" &
-done
+	wait
 
-wait
-echo "==================== ARCHIVING COMPLETED! ===================="
+	echo "=================================="
+	echo "Archiving Completed"
+	echo "=================================="
+}
+
+echo "=================================="
+echo "Archiving:"
+echo "$@"
+echo "=================================="
+
+read -rp "Start archiving? (Y/n)" answer
+action=${answer:-Y}
+
+case $action in
+[Yy])
+	run "$@"
+	;;
+[Nn])
+	exit 0
+	;;
+*)
+	echo "Wrong choice"
+	exit 1
+	;;
+esac
