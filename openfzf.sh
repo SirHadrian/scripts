d478b1b62cabcf7c1fe1171bd8aa498179875485
Toma Adrian <sirhadrian21@gmail.com>
2023-06-02 17:18:58 +0300

    fzf find and open with provided command

---
+++ openfzf.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+
+COMMAND=
+while getopts c:h opt; do
+	case "$opt" in
+	c)
+		COMMAND=$OPTARG
+		break
+		;;
+	*)
+		printf "%s" "Unknown option: $opt"
+		break
+		;;
+	esac
+done
+
+selection="$(fzf)"
+if [ -n "$selection" ]; then
+	# ${COMMAND//\{\}/"$selection"} &
+	$COMMAND "$selection" &>/dev/null &
+fi
