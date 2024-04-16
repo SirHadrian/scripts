eec53f6167d318354e52c4b5bbfb60a9a552fa4d
Toma Adrian Ionut <sirhadrian21@gmail.com>
2024-04-16 16:45:45 +0300

    trash restore

--- trash.sh
+++ trash.sh
@@ -7,8 +7,17 @@ then
         mkdir -p "$TRASH"
 fi
 
+if [[ "$1" == "restore" ]]
+then
+        FILE="$(basename "$2")"
+        mv -v "$TRASH/${FILE%.trashinfo}" "$(cat $TRASH/$FILE)"
+        rm "$TRASH/$FILE"
+
+        exit 0
+fi
+
 for file in "$@"
 do
         realpath "$file" > "$TRASH/${file%/}.trashinfo"
-        mv "$file" "$TRASH"
+        mv -v "$file" "$TRASH"
 done
--- wallpaper.sh
+++ wallpaper.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/bash
 
 usage(){
-	echo "Execute a commnad n times"
-	echo "-n | --time - duration in seconds"
-	echo "-e | --exec - commnad to execute every n times"
-	echo "-h | --help - print usage"
-	return
+        echo "Execute a command n times"
+        echo "-n | --time - duration in seconds"
+        echo "-e | --exec - command to execute every n times"
+        echo "-h | --help - print usage"
+        return
 }
 
 sleep_time=
@@ -13,41 +13,41 @@ artist=
 
 while [[ -n "$1" ]]
 do
-	case "$1" in
-		-n | --time)
-			shift
-			sleep_time=$1
-			;;
-		-e | --exec)
-			shift
-			artist="$1"
-			;;
-		-h | --help)
-			usage
-			exit
-			;;
-		*)
-			usage >&2
-			exit 1
-			;;
-	esac
-	shift
+        case "$1" in
+                -n | --time)
+                        shift
+                        sleep_time=$1
+                        ;;
+                -e | --exec)
+                        shift
+                        artist="$1"
+                        ;;
+                -h | --help)
+                        usage
+                        exit
+                        ;;
+                *)
+                        usage >&2
+                        exit 1
+                        ;;
+        esac
+        shift
 done
 
 if [[ -z "$sleep_time" || -z "$artist" ]]
 then
-	usage >&2
-	exit 1
+        usage >&2
+        exit 1
 fi
 
 while true
 do
-  # Get random image from artist dir
-  image="$(fd --type f --extension png --extension jpg --extension jpeg . $artist | shuf -n1)"
-  # Set background
-	feh --bg-max "$image"
-  # Write image path to file
-  echo "$image" > ~/.wallpaper
-  # Wait n seconds
-	sleep $sleep_time
+        # Get random image from artist dir
+        image="$(fd --type f --extension png --extension jpg --extension jpeg . "$artist" | shuf -n1)"
+        # Set background
+        feh --bg-max "$image"
+        # Write image path to file
+        echo "$image" > ~/.wallpaper
+        # Wait n seconds
+        sleep "$sleep_time"
 done
